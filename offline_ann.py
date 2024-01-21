127659c9c917ce899b796acb79efa943be00846a
w5688414 <w5688414@gmail.com>
2024-01-21 13:27:32 +0000

    Update offline ann

--- offline_ann.py
+++ offline_ann.py
@@ -1,14 +1,15 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
-from tools.utils import build_index
+from tools.utils import build_index, FaissSearch
 
 
 if __name__ == "__main__":
     embeddings = OpenAIEmbeddings(deployment="text-embedding-ada")
+    
     db = build_index(
         "faiss_abstract_baidu_openai",
         embeddings=embeddings,
         path="abstract_corpus_baidu_openai.json",
         abstract=True,
     )
-
-    print(db.search("what is the meaning of life", 1))
+    faiss_tool = FaissSearch(db, embeddings)
+    print(faiss_tool.search("what is the meaning of life", 1))
